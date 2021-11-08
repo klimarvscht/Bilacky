3ba430724338c5e615fe4026b80b5d249eee87f6
klimarvscht <93933609+klimarvscht@users.noreply.github.com>
2021-11-08 16:55:53 +0100

    prvni ulozena verze programu na bilancky
    - Neznama
    - Proudy

---
+++ BilancniVypocty/BilancniVypocty/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BilancniVypocty
+{
+    public partial class Form1 : Form
+    {
+        public static Button[] vychoziProudy;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Proud[] karel = new Proud[5];
+
+            for (int i = 0; i < karel.Length; i++)
+            {
+                karel[i] = new Proud(6, karel.Length, i);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VygenerovatProudyVychozi()
+        {
+
+        }
+    }
+}
---
+++ BilancniVypocty/BilancniVypocty/Neznama.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilancniVypocty
+{
+    class Neznama
+    {
+        public float max;
+        public float min;
+        public float value;
+        public bool known;
+        public string jmeno;
+        public string jednotka;
+        public int indexProudu;
+        public int indexSlozky;
+
+        public Neznama(float maximum, float minimum, string jmeno, string jednotka, int indexProudu,int indexSlozky)
+        {
+            this.max = maximum;
+            this.min = minimum;
+            this.jmeno = jmeno;
+            this.indexProudu = indexProudu;
+            this.indexSlozky = indexSlozky;
+            this.jednotka = jednotka;
+            known = false;
+            if (minimum > 0)
+            {
+                value = minimum;
+            }
+            else
+            {
+                value = 0;
+            }
+        }
+    }
+}
