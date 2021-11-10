912423a532923a502f536b17a13aa4e0f8dae665
klimar <93933609+klimarvscht@users.noreply.github.com>
2021-11-10 21:27:31 +0100

    Přidán řešič lineárních rovnic

--- BilancniVypocty/BilancniVypocty/Form1.cs
+++ BilancniVypocty/BilancniVypocty/Form1.cs
@@ -12,8 +12,6 @@ namespace BilancniVypocty
 {
     public partial class Form1 : Form
     {
-        public static Button[] vychoziProudy;
-        
         public Form1()
         {
             InitializeComponent();
@@ -21,11 +19,23 @@ namespace BilancniVypocty
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Proud[] karel = new Proud[5];
+            ReseniSoustavyRovnic.linearniMatice = new float[4][];
 
-            for (int i = 0; i < karel.Length; i++)
+            ReseniSoustavyRovnic.linearniMatice[0] = new float[]{ 4, 3, 6 ,7};
+            ReseniSoustavyRovnic.linearniMatice[1] = new float[] { 2, 4, 8, 9 };
+            ReseniSoustavyRovnic.linearniMatice[2] = new float[] { 2, -3, 0, -9 };
+            ReseniSoustavyRovnic.linearniMatice[3] = new float[] { 5, 1, 3, -3 };
+            ReseniSoustavyRovnic.vysledkyLinearni = new float[] { 0, 1, 2, -3};
+
+            ReseniSoustavyRovnic.UpravaLinearniRovnice();
+
+            for (int i = 0; i < ReseniSoustavyRovnic.linearniMatice.Length; i++)
             {
-                karel[i] = new Proud(6, karel.Length, i);
+                for (int j = 0; j < ReseniSoustavyRovnic.linearniMatice[i].Length; j++)
+                {
+                    Console.Write(ReseniSoustavyRovnic.linearniMatice[i][j] + "; ");
+                }
+                Console.WriteLine("= " + ReseniSoustavyRovnic.vysledkyLinearni[i]);
             }
         }
 
